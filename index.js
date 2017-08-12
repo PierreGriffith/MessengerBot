d1851dc5f4958b9c8501271b063f5f0a83998726
PierreGriffith <parisianpierre@yahoo.com>
2017-08-12 18:19:07 +0200

    correction

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-const Bot = require('messenger-bot')
+
 const request = require('request')
 const express = require('express')
 const bodyParser = require("body-parser");
-const http = require('http')
 
-let app = express()
+/*
 let bot = new Bot({
   token: 'EAALViQNc1LgBADkj9bPZBjFwaHZB70tkloPYbgZBZAtUki8edZB8SU72rcJT2F6ZB3DnYFmdm6SlZCRkk9BJTYZCoigvrdpX2puTCvDom8lFucRnWRKGJ5B1rPZBeWjx1ilY8ecibTVt5E9Nli4bZAa3MvqAZCLwnCwS6pOW7eSOuKAlgZDZD',
   verify: process.env.VERIFICATION_TOKEN,
   app_secret: '1b6570300baa8ce5cfdcd349167a1a73'
 })
+*/
 
 
-bot.on('error', (err) => {
-  console.log(err.message)
-})
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
 
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
-
-    reply({ text }, (err) => {
-      if (err) throw err
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
-  })
-})
-
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-app.get('/', (req, res) => {
-  return bot._verify(req, res)
-})
-
-app.post('/', (req, res) => {
-  bot._handleMessage(req.body)
-  res.end(JSON.stringify({status: 'ok'}))
-})
-
-
-http.createServer(bot.middleware()).listen(5000)
-console.log('Echo bot server running at port 5000.')
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
