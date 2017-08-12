48ecc7207799bed86a72c4511f20e968ab7eb138
PierreGriffith <parisianpierre@yahoo.com>
2017-08-12 18:01:00 +0200

    correction

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const Bot = require('messenger-bot')
 const request = require('request')
 const express = require('express')
 const bodyParser = require("body-parser");
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
+const http = require('http')
 
 
 let bot = new Bot({
@@ -14,22 +10,6 @@ let bot = new Bot({
   verify: 'this_is_my_token',
   app_secret: '1b6570300baa8ce5cfdcd349167a1a73'
 })
-
-
-app.get("/", function (req, res) {
-  res.send("Deployed!");
-});
-
-
-app.get("/webhook", function (req, res) {
-      if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
-  } else {
-    console.error("Verification failed. The tokens do not match.");
-    res.sendStatus(403);
-  }
-});
 
 
 bot.on('error', (err) => {
@@ -49,3 +29,6 @@ bot.on('message', (payload, reply) => {
     })
   })
 })
+
+http.createServer(bot.middleware()).listen(3000)
+console.log('Echo bot server running at port 3000.')
