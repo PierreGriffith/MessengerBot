dca4668131d989c70d8b24b0847f20a0d3b43479
PierreGriffith <parisianpierre@yahoo.com>
2017-08-12 17:26:49 +0200

    correction

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.get("/", function (req, res) {
 });
 
 app.get("/webhook", function (req, res) {
+    
+console.log(process.env.VERIFICATION_TOKEN + "  MDR FDP")
   if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
@@ -26,5 +28,4 @@ app.get("/webhook", function (req, res) {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
   }
-    console.log(process.env.VERIFICATION_TOKEN)
 });
