d087e5b12ae8c185b71c66ec9c47100daa5960e2
PierreGriffith <parisianpierre@yahoo.com>
2017-08-12 16:42:20 +0200

    correction

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const http = require('http')
 const Bot = require('messenger-bot')
+const request = require('request')
+const express = require('express')
+const bodyParser = require("body-parser");
 
-let bot = new Bot({
-  token: 'EAALViQNc1LgBAKHgvRZCH5ZASsXVu4jyBU8QKuhqum1y1H8CzlZAcrIHrEDAcZCETMh5MXAiZAZCpsPMrtoNgF0YDTzn1Gu7Ppc0EgqW6tg8bKXaYjMYgE83H1dQBQZAj05RJG8VbRykEVZAkexEI7uzYWZBsjmDO5IdD58gB3JZAQuQZDZD',
-  verify: 'VERIFY_TOKEN',
-  app_secret: 'APP_SECRET'
-})
 
-bot.on('error', (err) => {
-  console.log(err.message)
-})
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
 
-    reply({ text }, (err) => {
-      if (err) throw err
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
-  })
-})
-
-http.createServer(bot.middleware()).listen(3000)
 console.log('Echo bot server running at port 3000.')
+
+
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
