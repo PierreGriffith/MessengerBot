f9e5eccb8b17d9846694c07717686078cbb5a951
PierreGriffith <parisianpierre@yahoo.com>
2017-08-13 14:21:27 +0200

    l

--- index.js
+++ index.js
@@ -25,23 +25,20 @@ app.get("/webhook", function (req, res) {
 });
 
 
-// All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function (req, res) {
   // Make sure this is a page subscription
-    if (req.body.object == "page") 
-  {
+  if (req.body.object == "page") {
     // Iterate over each entry
     // There may be multiple entries if batched
     req.body.entry.forEach(function(entry) {
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-        
-          if (event.postback) {
+        if (event.postback) {
           processPostback(event);
         }
       });
     });
-            
+
     res.sendStatus(200);
   }
 });
