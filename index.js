c46ccb9f5fec32b9f89888abbff27a26bee53b8d
PierreGriffith <parisianpierre@yahoo.com>
2017-08-14 18:43:57 +0200

    l

--- index.js
+++ index.js
@@ -186,9 +186,10 @@ function send_ordinateur(recipient_id, res)
     Ordi.find({name : res}, function(err, resp) 
             {
         
-    console.log(resp)  
-    console.log(resp[0].name)
-    console.log(resp[0].link)
+    console.log(resp.get("name"))  
+//    console.log(resp[0].name)
+   // console.log(resp[0].link)
+        
     var buttons =  [
        { 
         "type":"web_url",
