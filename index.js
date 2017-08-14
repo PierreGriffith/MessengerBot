bb4aacf2b190b50f0e24560003436ccdf37251f1
PierreGriffith <parisianpierre@yahoo.com>
2017-08-14 21:04:16 +0200

    l

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ function send_ordinateur(recipient_id, res)
                 
         var link = JSON.stringify(resp) 
         
-        link = link.split(/"/)[6]
+        link = link.split(/"/)[8]
         console.log(link)
        
     var buttons =  [
