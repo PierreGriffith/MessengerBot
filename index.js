298a4bcf0d8b223bc339f52f60020ab8a66853e9
PierreGriffith <parisianpierre@yahoo.com>
2017-08-14 19:47:31 +0200

    l

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ function send_ordinateur(recipient_id, res)
         console.log(resp.link)
         */
         var obj = JSON.parse(link)
-        console.log(obj.link)
+        console.log(obj)
         
         
         
